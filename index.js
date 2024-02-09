f9d89e5d6f4e1ae26d644135ba96f40ab5e6a360
rethabile <rethabilemakaotsi17@gmail.com>
2024-02-09 16:15:56 +0200

    express

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Welcome to Express JS");
+import express from 'express'
+
+// create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+
+// Router
+app.get('/', (req, res)=>{
+    // res.status(200).json({
+    //    msg: 'you\'re home' 
+    res.json({
+        status: res.statusCode,
+        msg: 'you\'re home'
+    })
+    })
+    app.get('/about', (req, res)=>{
+        
+        res.json({
+            status: res.statusCode,
+            msg: 'about Page'
+        })
+        })
+        app.all('*', (req, res)=>{
+        
+            res.json({
+                status: 404,
+                msg: '404 page'
+            })
+            })
+
+
+
+app.listen(port)
